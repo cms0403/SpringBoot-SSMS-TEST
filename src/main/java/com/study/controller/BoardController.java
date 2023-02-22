032d899734b6e52f9624af64360f98046976ea1b
cmsjy040317 <cmsjy040317@naver.com>
2023-02-22 17:37:07 +0900

    first commit

---
+++ src/main/java/com/study/controller/BoardController.java
@@ -0,0 +1,23 @@
+package com.study.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BoardController {
+
+    @GetMapping("/")
+    public String list() {
+        return "board/list.html";
+    }
+
+    @GetMapping("/post")
+    public String post() {
+        return "board/post.html";
+    }
+
+    @GetMapping("/test")
+    public String test() {
+        return "board/test.html";
+    }
+}
